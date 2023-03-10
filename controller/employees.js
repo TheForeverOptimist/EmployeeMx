7bd88b1bd10721d867fb0b4341ce18083a3c6241
TheForeverOptimist <sean@elevate.app>
2023-03-10 13:50:30 -0500

    updates to controller

--- controller/employees.js
+++ controller/employees.js
@@ -14,52 +14,52 @@ router.post('/employees', async (req, res) => {
 });
 
 // Route to get all employees
-router.get('/supercars', async (req, res) => {
+router.get('/employees', async (req, res) => {
   try {
-    const supercars = await Supercar.find();
-    res.json(supercars);
+    const employees = await Employee.find();
+    res.json(employees);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
-// Route to get a single supercar by ID
-router.get('/supercars/:id', async (req, res) => {
+// Route to get a single employee by ID
+router.get('/employees/:id', async (req, res) => {
   try {
-    const supercar = await Supercar.findById(req.params.id);
-    if (!supercar) {
-      return res.status(404).json({ message: 'Supercar not found' });
+    const employees = await Employee.findById(req.params.id);
+    if (!employees) {
+      return res.status(404).json({ message: 'Employee not found' });
     }
-    res.json(supercar);
+    res.json(employees);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
-// Route to update a supercar by ID
+// Route to update a employee by ID
 router.patch('/supercars/:id', async (req, res) => {
   try {
-    const supercar = await Supercar.findByIdAndUpdate(req.params.id, req.body, {
+    const employees = await Employee.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
       runValidators: true
     });
-    if (!supercar) {
+    if (!employees) {
       return res.status(404).json({ message: 'Supercar not found' });
     }
-    res.json(supercar);
+    res.json(employees);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
-// Route to delete a supercar by ID
+// Route to delete a employee by ID
 router.delete('/supercars/:id', async (req, res) => {
   try {
-    const supercar = await Supercar.findByIdAndDelete(req.params.id);
-    if (!supercar) {
-      return res.status(404).json({ message: 'Supercar not found' });
+    const employees = await Employee.findByIdAndDelete(req.params.id);
+    if (!employees) {
+      return res.status(404).json({ message: 'Employee not found' });
     }
-    res.json({ message: 'Supercar deleted' });
+    res.json({ message: 'Employee deleted' });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
